dd512a49b0d984ad8bc9b35c1e1dc0529a32632f
witoldzol <blue3656@gmail.com>
2019-02-09 18:32:12 +0000

    update

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -2,7 +2,7 @@ import  { Component } from "@angular/core"
 
 @Component({
     selector: 'pm-products',
-    templateUrl: './products/product-list.component.html'
+    templateUrl: './product-list.component.html'
 })
 
 export class ProductListComponent {
